11d69de628ba1dcbf7647db27ae5582fa1edabe2
Ravindran K <ravindran.k@gmail.com>
2025-04-04 20:01:02 +0530

    Create qs.js

---
+++ qs.js
@@ -0,0 +1,9 @@
+import qs from 'qs';
+const data = { 'bar': 123 };
+const options = {
+  method: 'POST',
+  headers: { 'content-type': 'application/x-www-form-urlencoded' },
+  data: qs.stringify(data),
+  url,
+};
+axios(options);
